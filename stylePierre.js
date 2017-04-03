c7d0bd0921788f5d55f15e4bc4750d175ebd2ef5
Vautherin <VautherinP@science.regn.net>
2017-04-03 17:38:43 +0200

    styling components using React instead of css

---
+++ stylePierre.js
@@ -0,0 +1,14 @@
+
+var stylePierre = {
+              marginLeft: 60,
+              border: "1px solid green",
+              };
+
+var styles = {
+                width: 180,
+                height: 180,
+                marginLeft: 30,
+                border: "1px solid red",
+                };
+
+module.exports = {stylePierre, styles};
